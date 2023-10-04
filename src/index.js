d70ea17d93e3d8d49e8dbc8353ffb91d63044b1c
Sergei_Kuznetsov <SKuznetsov@asist-lab.com>
2023-10-04 14:18:39 +0300

    add layout of app with two js files and webpack config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+// вне ноды require работать не будет, поэтому используем импорты
+import { searchArt } from "./art_helper.js";
+
 window.onload = (ev) => {
   init();
 };
 
 const init = () => {
-  console.log("hello I am a sample index.js");
+  console.log("hello I am a sample index.js vn");
+  document
+    .getElementById("title-search-button")
+    .addEventListener("click", handleSearchClick);
+};
+
+const handleSearchClick = (ev) => {
+  const title = document.getElementById("title-search-input").value;
+  searchArt({ title }).then(renderResults);
+};
+
+const renderResults = ({ artPieces }) => {
+  console.log(artPieces);
+  document.getElementById("search-results").innerHTML = artPieces.join("");
 };
